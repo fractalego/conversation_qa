3adc1e4e526a54a6a2b7780273c0c6dc98530edc
Fractalego <albertocetoli@gmail.com>
2022-09-03 11:49:18 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="conversation-qa",
-    version="0.0.2",
+    version="0.0.3",
     url="http://github.com/fractalego/conversation_qa",
     author="Alberto Cetoli",
     author_email="alberto@fractalego.io",
